55f45c93a4c627de272bdf1820b2d51e45781dee
ToxicVasiliy <44618834+ToxicVasiliy@users.noreply.github.com>
2020-10-21 22:18:03 +0500

    Trying to make it work
     ._.

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import *
 import os
-from subprocess import run
+from time import sleep
+import subprocess
 
-d = open('programm.txt', mode='wt')
-cmd = [ 'echo', 'arg1', 'arg2' ]
 
 class MyWidget(QMainWindow):
     def __init__(self):
@@ -15,6 +14,8 @@ class MyWidget(QMainWindow):
         self.pushButton.clicked.connect(self.compile)
 
     def compile(self):
+        arr = []
+        d = open('programm.txt', mode='wt')
         doc = self.textEdit1.document()
         block = doc.begin()
         lines = [block.text()]
@@ -22,17 +23,33 @@ class MyWidget(QMainWindow):
             block = block.next()
         lines.append(block.text())
         lines = self.textEdit1.toPlainText().split('\n')
-        d.write('from time import sleep' + '\n')
-
         for _ in lines:
-            print(_)
+            # print(_)
             d.write(_)
             d.write('\n')
-        d.write('sleep(100)')
         d.close()
         os.rename(r'programm.txt', r'programm.py')
-        os.system('python programm.py')
-        self.labebl.display(run("pwd", capture_output=True).stdout)
+        # os.system('python programm.py')
+        try:
+            a = subprocess.check_output('programm.py', shell=True)
+        except Exception:
+            print('error')
+        print(arr)
+        tf = 3
+        for _ in a:
+            if tf == 3:
+                print(_, end='')
+                if a.index(_) != 0 and a.index(_) != 1 and a.index(_) != -1 and a.index(_) != -2:
+                    if _ == "\r" and a[a.index(_) + 1] == 'n' and a[a.index(_) + 3] == "\r":
+                        print()
+                        tf = 0
+            elif tf < 3:
+                tf += 1
+
+        # arr = arr.split("r")
+        # print(arr)
+        os.remove('programm.py')
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
