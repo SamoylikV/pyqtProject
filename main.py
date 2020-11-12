a5716762550566f82e03d27efda998a44cdb3dcf
ToxicVasiliy <44618834+ToxicVasiliy@users.noreply.github.com>
2020-11-12 17:14:42 +0500

    Оно работает лол

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import sys, random
 from score import MyWidget
-from hez import ClssDialog
 from PyQt5.QtWidgets import QMainWindow, QFrame, QDesktopWidget, QApplication
 from PyQt5.QtCore import Qt, QBasicTimer, pyqtSignal
 from PyQt5.QtGui import QPainter, QColor
@@ -14,52 +13,48 @@ class Tetris(QMainWindow):
 
     def __init__(self):
         super().__init__()
+        self.main_window()
 
-        self.initUI()
-
-    def initUI(self):
+    def main_window(self):
         """
-        инициализация главного окна
+        создание главного окна
         """
-        self.tboard = Board(self)
-        self.setCentralWidget(self.tboard)
+        self.tetrist_list = tetrist_list(self)
+        self.setCentralWidget(self.tetrist_list)
 
         self.statusbar = self.statusBar()
-        self.tboard.msg2Statusbar[str].connect(self.statusbar.showMessage)
+        self.tetrist_list.to_Statubar[str].connect(self.statusbar.showMessage)
 
-        self.tboard.start()
+        self.tetrist_list.start()
 
         self.resize(180, 380)
-        self.center()
+        screen = QDesktopWidget().screenGeometry()
+        size = self.geometry()
+        self.move((screen.width() - size.width()) // 2,
+                  (screen.height() - size.height()) // 2)
         self.setWindowTitle('Tetris')
         self.show()
-
-    def center(self):
-        """
-        располагает игру по центру относительно экрана
-        """
         screen = QDesktopWidget().screenGeometry()
         size = self.geometry()
         self.move((screen.width() - size.width()) // 2,
                   (screen.height() - size.height()) // 2)
 
 
-class Board(QFrame):
+class tetrist_list(QFrame):
     """
     поле игры
     """
-    msg2Statusbar = pyqtSignal(str)
-
-    BoardWidth = 10
-    BoardHeight = 22
-    Speed = 300
+    to_Statubar = pyqtSignal(str)
+    tetrist_list_Width = 10
+    tetrist_list_Height = 20
+    Speed = 150
 
     def __init__(self, parent):
         super().__init__(parent)
 
-        self.initBoard()
+        self.init_tetrist_list()
 
-    def initBoard(self):
+    def init_tetrist_list(self):
 
         self.timer = QBasicTimer()
         self.isWaitingAfterLine = False
@@ -67,24 +62,30 @@ class Board(QFrame):
         self.curX = 0
         self.curY = 0
         self.numLinesRemoved = 0
-        self.board = []
+        self.tetrist_list = []
 
         self.setFocusPolicy(Qt.StrongFocus)
         self.isStarted = False
         self.isPaused = False
-        self.clearBoard()
+        self.clear_tetrist_list()
 
-    def shapeAt(self, x, y):
-        return self.board[(y * Board.BoardWidth) + x]
+    def figure_Type(self, x, y):
+        '''
+        определяет тип фигуры в данном блоке.
+        '''
+        return self.tetrist_list[(y * tetrist_list.tetrist_list_Width) + x]
 
-    def setShapeAt(self, x, y, shape):
-        self.board[(y * Board.BoardWidth) + x] = shape
+    def set_Size(self, x, y, shape):
+        '''
+        задаём размер поля
+        '''
+        self.tetrist_list[(y * tetrist_list.tetrist_list_Width) + x] = shape
 
     def squareWidth(self):
-        return self.contentsRect().width() // Board.BoardWidth
+        return self.contentsRect().width() // tetrist_list.tetrist_list_Width
 
     def squareHeight(self):
-        return self.contentsRect().height() // Board.BoardHeight
+        return self.contentsRect().height() // tetrist_list.tetrist_list_Height
 
     def start(self):
         """
@@ -93,15 +94,18 @@ class Board(QFrame):
         if self.isPaused:
             return
 
+        wave_obj = sa.WaveObject.from_wave_file("theme.wav")
+        play_obj = wave_obj.play()
+
         self.isStarted = True
         self.isWaitingAfterLine = False
         self.numLinesRemoved = 0
-        self.clearBoard()
+        self.clear_tetrist_list()
 
-        self.msg2Statusbar.emit(str(self.numLinesRemoved))
+        self.to_Statubar.emit(str(self.numLinesRemoved))
 
-        self.newPiece()
-        self.timer.start(Board.Speed, self)
+        self.next_Figure()
+        self.timer.start(tetrist_list.Speed, self)
 
     def pause(self):
         """
@@ -114,11 +118,11 @@ class Board(QFrame):
 
         if self.isPaused:
             self.timer.stop()
-            self.msg2Statusbar.emit("paused")
+            self.to_Statubar.emit("paused")
 
         else:
-            self.timer.start(Board.Speed, self)
-            self.msg2Statusbar.emit(str(self.numLinesRemoved))
+            self.timer.start(tetrist_list.Speed, self)
+            self.to_Statubar.emit(str(self.numLinesRemoved))
 
         self.update()
 
@@ -130,32 +134,32 @@ class Board(QFrame):
         painter = QPainter(self)
         rect = self.contentsRect()
 
-        boardTop = rect.bottom() - Board.BoardHeight * self.squareHeight()
+        tetrist_listTop = rect.bottom() - tetrist_list.tetrist_list_Height * self.squareHeight()
 
-        for i in range(Board.BoardHeight):
-            for j in range(Board.BoardWidth):
-                shape = self.shapeAt(j, Board.BoardHeight - i - 1)
+        for i in range(tetrist_list.tetrist_list_Height):
+            for j in range(tetrist_list.tetrist_list_Width):
+                shape = self.figure_Type(j, tetrist_list.tetrist_list_Height - i - 1)
 
-                if shape != Tetrominoe.NoShape:
+                if shape != figure_list.NoShape:
                     self.drawSquare(painter,
                                     rect.left() + j * self.squareWidth(),
-                                    boardTop + i * self.squareHeight(), shape)
+                                    tetrist_listTop + i * self.squareHeight(), shape)
 
-        if self.curPiece.shape() != Tetrominoe.NoShape:
+        if self.curPiece.shape() != figure_list.NoShape:
 
             for i in range(4):
                 x = self.curX + self.curPiece.x(i)
                 y = self.curY - self.curPiece.y(i)
                 self.drawSquare(painter, rect.left() + x * self.squareWidth(),
-                                boardTop + (Board.BoardHeight - y - 1) * self.squareHeight(),
+                                tetrist_listTop + (tetrist_list.tetrist_list_Height - y - 1) * self.squareHeight(),
                                 self.curPiece.shape())
 
     def keyPressEvent(self, event):
         """
         обработка нажатий на клавиатурру для поворота фигурок
         """
-        if not self.isStarted or self.curPiece.shape() == Tetrominoe.NoShape:
-            super(Board, self).keyPressEvent(event)
+        if not self.isStarted or self.curPiece.shape() == figure_list.NoShape:
+            super(tetrist_list, self).keyPressEvent(event)
             return
 
         key = event.key()
@@ -167,17 +171,19 @@ class Board(QFrame):
         if self.isPaused:
             return
 
+        # рисование упавших вниз частей.
+
         elif key == Qt.Key_Left:
-            self.tryMove(self.curPiece, self.curX - 1, self.curY)
+            self.figure_Move(self.curPiece, self.curX - 1, self.curY)
 
         elif key == Qt.Key_Right:
-            self.tryMove(self.curPiece, self.curX + 1, self.curY)
+            self.figure_Move(self.curPiece, self.curX + 1, self.curY)
 
         elif key == Qt.Key_Down:
-            self.tryMove(self.curPiece.rotateRight(), self.curX, self.curY)
+            self.figure_Move(self.curPiece.rotateRight(), self.curX, self.curY)
 
         elif key == Qt.Key_Up:
-            self.tryMove(self.curPiece.rotateLeft(), self.curX, self.curY)
+            self.figure_Move(self.curPiece.rotateLeft(), self.curX, self.curY)
 
         elif key == Qt.Key_Space:
             self.dropDown()
@@ -186,30 +192,30 @@ class Board(QFrame):
             self.oneLineDown()
 
         else:
-            super(Board, self).keyPressEvent(event)
+            super(tetrist_list, self).keyPressEvent(event)
 
     def timerEvent(self, event):
         """
-        фунцкия спуска фигурок
+        фсоздаём новую фигуру и передвигаем падающую
         """
 
         if event.timerId() == self.timer.timerId():
 
             if self.isWaitingAfterLine:
                 self.isWaitingAfterLine = False
-                self.newPiece()
+                self.next_Figure()
             else:
                 self.oneLineDown()
 
         else:
-            super(Board, self).timerEvent(event)
+            super(tetrist_list, self).timerEvent(event)
 
-    def clearBoard(self):
+    def clear_tetrist_list(self):
         """
-        очистить поле
+        очистка поля путём помещения путой фигуры
         """
-        for i in range(Board.BoardHeight * Board.BoardWidth):
-            self.board.append(Tetrominoe.NoShape)
+        for i in range(tetrist_list.tetrist_list_Height * tetrist_list.tetrist_list_Width):
+            self.tetrist_list.append(figure_list.NoShape)
 
     def dropDown(self):
         """
@@ -219,44 +225,44 @@ class Board(QFrame):
 
         while newY > 0:
 
-            if not self.tryMove(self.curPiece, self.curX, newY - 1):
+            if not self.figure_Move(self.curPiece, self.curX, newY - 1):
                 break
 
             newY -= 1
 
-        self.pieceDropped()
+        self.figure_Down()
 
     def oneLineDown(self):
 
-        if not self.tryMove(self.curPiece, self.curX, self.curY - 1):
-            self.pieceDropped()
+        if not self.figure_Move(self.curPiece, self.curX, self.curY - 1):
+            self.figure_Down()
 
-    def pieceDropped(self):
+    def figure_Down(self):
         """
-        вызывается после достижения фигурки низа поля
+        вызывается после достижения фигурки низ
         """
         for i in range(4):
             x = self.curX + self.curPiece.x(i)
             y = self.curY - self.curPiece.y(i)
-            self.setShapeAt(x, y, self.curPiece.shape())
+            self.set_Size(x, y, self.curPiece.shape())
 
-        self.removeFullLines()
+        self.line_Remove()
 
         if not self.isWaitingAfterLine:
-            self.newPiece()
+            self.next_Figure()
 
-    def removeFullLines(self):
+    def line_Remove(self):
         """
         вызывается при сборе полной линии от края до края поля и удаляет ее
         """
         numFullLines = 0
         rowsToRemove = []
 
-        for i in range(Board.BoardHeight):
+        for i in range(tetrist_list.tetrist_list_Height):
 
             n = 0
-            for j in range(Board.BoardWidth):
-                if not self.shapeAt(j, i) == Tetrominoe.NoShape:
+            for j in range(tetrist_list.tetrist_list_Width):
+                if not self.figure_Type(j, i) == figure_list.NoShape:
                     n = n + 1
 
             if n == 10:
@@ -266,39 +272,43 @@ class Board(QFrame):
 
         for m in rowsToRemove:
 
-            for k in range(m, Board.BoardHeight):
-                for l in range(Board.BoardWidth):
-                    self.setShapeAt(l, k, self.shapeAt(l, k + 1))
+            for k in range(m, tetrist_list.tetrist_list_Height):
+                for l in range(tetrist_list.tetrist_list_Width):
+                    self.set_Size(l, k, self.figure_Type(l, k + 1))
 
         numFullLines = numFullLines + len(rowsToRemove)
 
         if numFullLines > 0:
             self.numLinesRemoved = self.numLinesRemoved + numFullLines
-            self.msg2Statusbar.emit(str(self.numLinesRemoved))
+            self.to_Statubar.emit(str(self.numLinesRemoved))
 
             self.isWaitingAfterLine = True
-            self.curPiece.setShape(Tetrominoe.NoShape)
+            self.curPiece.set_Shape(figure_list.NoShape)
             self.update()
 
     def call_score(self, score_val):
+        """
+        Показывает счёт
+        """
+
         dialog = MyWidget(score_val, self)
         dialog.exec_()
 
-    def newPiece(self):
+    def next_Figure(self):
         """
-        задаем новую фигурку и еще функция пройгрыша
+        создаёт новую фигуру, а если для неё нет места, то пройгрыш
         """
         self.curPiece = Shape()
-        self.curPiece.setRandomShape()
-        self.curX = Board.BoardWidth // 2 + 1
-        self.curY = Board.BoardHeight - 1 + self.curPiece.minY()
+        self.curPiece.set_Random_Figure()
+        self.curX = tetrist_list.tetrist_list_Width // 2 + 1
+        self.curY = tetrist_list.tetrist_list_Height - 1 + self.curPiece.minY()
 
-        if not self.tryMove(self.curPiece, self.curX, self.curY):
+        if not self.figure_Move(self.curPiece, self.curX, self.curY):
             wave_obj = sa.WaveObject.from_wave_file("lose.wav")
             play_obj = wave_obj.play()
-            self.msg2Statusbar.emit("Game over")
+            self.to_Statubar.emit("Game over")
             self.call_score(int(self.numLinesRemoved))
-            self.curPiece.setShape(Tetrominoe.NoShape)
+            self.curPiece.set_Shape(figure_list.NoShape)
             self.timer.stop()
             self.isStarted = False
             # app = QApplication(sys.argv)
@@ -306,23 +316,22 @@ class Board(QFrame):
             # ex.exec_()
             # sys.exit(app.exec())
 
-
-    def tryMove(self, newPiece, newX, newY):
+    def figure_Move(self, next_Figure, newX, newY):
         """
-        поворачивает фигурки на поле
+        перемещаем фигуру когда фигура не аходится на краю доски
         """
         for i in range(4):
 
-            x = newX + newPiece.x(i)
-            y = newY - newPiece.y(i)
+            x = newX + next_Figure.x(i)
+            y = newY - next_Figure.y(i)
 
-            if x < 0 or x >= Board.BoardWidth or y < 0 or y >= Board.BoardHeight:
+            if x < 0 or x >= tetrist_list.tetrist_list_Width or y < 0 or y >= tetrist_list.tetrist_list_Height:
                 return False
 
-            if self.shapeAt(x, y) != Tetrominoe.NoShape:
+            if self.figure_Type(x, y) != figure_list.NoShape:
                 return False
 
-        self.curPiece = newPiece
+        self.curPiece = next_Figure
         self.curX = newX
         self.curY = newY
         self.update()
@@ -353,9 +362,9 @@ class Board(QFrame):
                          y + self.squareHeight() - 1, x + self.squareWidth() - 1, y + 1)
 
 
-class Tetrominoe(object):
+class figure_list(object):
     """
-    количество видов фигур
+    Фигуры
     """
     NoShape = 0
     ZShape = 1
@@ -369,11 +378,7 @@ class Tetrominoe(object):
 
 class Shape(object):
     """
-    класс фигур
-    """
-
-    """
-    Сами фигуры
+    Фигуры
     """
 
     coordsTable = (
@@ -390,14 +395,14 @@ class Shape(object):
     def __init__(self):
 
         self.coords = [[0, 0] for i in range(4)]
-        self.pieceShape = Tetrominoe.NoShape
+        self.pieceShape = figure_list.NoShape
 
-        self.setShape(Tetrominoe.NoShape)
+        self.set_Shape(figure_list.NoShape)
 
     def shape(self):
         return self.pieceShape
 
-    def setShape(self, shape):
+    def set_Shape(self, shape):
         """
         инициализация фигуры
         """
@@ -410,11 +415,11 @@ class Shape(object):
 
         self.pieceShape = shape
 
-    def setRandomShape(self):
+    def set_Random_Figure(self):
         """
         выбор случайной фигуры
         """
-        self.setShape(random.randint(1, 7))
+        self.set_Shape(random.randint(1, 7))
 
     """
     методы для работы с координатами
@@ -469,7 +474,7 @@ class Shape(object):
         поворот фигуры влево
         """
 
-        if self.pieceShape == Tetrominoe.SquareShape:
+        if self.pieceShape == figure_list.SquareShape:
             return self
 
         result = Shape()
@@ -485,7 +490,7 @@ class Shape(object):
         """
         поворот фигуры вправо
         """
-        if self.pieceShape == Tetrominoe.SquareShape:
+        if self.pieceShape == figure_list.SquareShape:
             return self
 
         result = Shape()
@@ -498,12 +503,8 @@ class Shape(object):
         return result
 
 
-
 if __name__ == '__main__':
     # запуск
     app = QApplication([])
     tetris = Tetris()
-    wave_obj = sa.WaveObject.from_wave_file("theme.wav")
-    play_obj = wave_obj.play()
     sys.exit(app.exec_())
-
